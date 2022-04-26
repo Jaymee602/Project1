2185364db89cbd482260b111ac467581bd0c7e39
Ruben <[connor.j.schulz@gmail.com]>
2022-04-26 19:47:30 -0400

    Made functions for pulling NYTimes popular articles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// returns NYTimes API info with given queries
+function getNYTimesJSON(queries) {
+    return fetch('https://api.nytimes.com/svc/' + queries)
+    .then((response)=>response.json())
+    .then((responseJson)=>{return responseJson});
+};
+
+async function popularArticles(numberArticle){
+    const json = await getNYTimesJSON("mostpopular/v2/viewed/1.json?api-key=NbSmqy6RxhsJd8JK3rbJalvWSMsf1mqu");
+    var topArticle = json.results[numberArticle].url;
+    var iframe = "<iframe src='" + topArticle + "' scrolling='no' frameborder='0' allowtransparency='true' title='Twitter Nears a Deal to Sell Itself to Elon Musk' style='border:none;max-width:500px;min-width:300px;min-height:550px;display:block;width:100%;'></iframe>"
+
+    console.log(json);
+};
+
 // returns Youtube API info with given queries
 function getYouTubeJSON(queries) {
     return fetch('https://youtube.googleapis.com/youtube/v3/' + queries)
@@ -14,7 +29,4 @@ async function popularVideo() {
     console.log(baseLink);            // hello is now available
 };
 
-
-
-
-popularVideo();
+popularArticles(0);
