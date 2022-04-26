486a7c9392b7b29b99efc8d049210362ad8e3f4d
Ruben <[connor.j.schulz@gmail.com]>
2022-04-26 17:11:37 -0400

    Split function into two, one to grab API data and the other one to grab that data, wait until the promise is finshed, and then return that data for the most popular video on youtube right now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-var array;
+// returns Youtube API info with given queries
+function getYouTubeJSON(queries) {
+    return fetch('https://youtube.googleapis.com/youtube/v3/' + queries)
+    .then((response)=>response.json())
+    .then((responseJson)=>{return responseJson});
+};
 
-var test = function(){
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=AIzaSyALijQA6Nkc_HNMR6qFP0T1IN-cAO0Il2o").then(function(response){
-        response.json().then(function(data){
-            console.log(data);
-            array = data.items;
-            console.log(array);
-        })
-    })
-}
+// returns link to the most popular video on youtube today
+async function popularVideo() {
+    const json = await getYouTubeJSON("videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=1&regionCode=US&key=AIzaSyALijQA6Nkc_HNMR6qFP0T1IN-cAO0Il2o");  // command waits until completion
+    var baseLink = "https://www.youtube.com/watch?v=";
+    var id = json.items[0].id;
+    baseLink += id;
+    console.log(baseLink);            // hello is now available
+};
 
-test();
+
+
+
+popularVideo();
