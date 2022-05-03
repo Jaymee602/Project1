811b6e8ec35c4bf9472d3bd65e68d1dd32d3dead
Ruben <[connor.j.schulz@gmail.com]>
2022-05-03 18:18:38 -0400

    added javascript files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// handling current time formatting
+
+var now = moment();
+var nowArray = [
+    now.get('year'),
+    now.get('month') + 1,
+    now.get('date') - 1
+];
+if(nowArray[1] < 10){
+    nowArray[1] = "0" + nowArray[1];
+}
+if(nowArray[2] < 10){
+    nowArray[2] = "0" + nowArray[2];
+}
+
 // ------- base api pulls -------
 
 // returns NYTimes API info with given queries
@@ -39,7 +54,43 @@ function makeNYIframe(baseURL){
     return URL;
 }
 
+async function NYTimesSearch(topic, search, startdate, enddate){
+    var queries = "search/v2/articlesearch.json?";
+    
+    if(startdate){
+        queries += "begin_date=" + startdate;
+    }
+    if(enddate){
+        if(startdate){
+            queries += "&";
+        }
+        queries += "end_date=";
+    }
 
+    if(queries){
+        if(startdate || enddate){
+            queries+="&"
+        };
+        queries += "fl=web_url";
+    }
+
+    if(topic){
+        queries += "&fq=section_name%3A%20(%22" + topic + "%22)";
+    }
+
+    if(search){
+        var array = search.split(" ");
+        queries += "&q=" + array[0];
+        for(var i = 1; i < array.length; i++){
+            queries += "%2B" + array[i];
+        }
+    }
+    queries += "&api-key=NbSmqy6RxhsJd8JK3rbJalvWSMsf1mqu";
+
+    const json = await getNYTimesJSON(queries);
+    var links = json.response.docs;
+    return links;
+};
 
 // ------- Youtube Functions -------
 
@@ -52,8 +103,31 @@ async function popularVideo() {
     return baseLink;
 };
 
+async function searchYoutube(topic, search){
+    var link = "search?part=snippet&maxResults=5&order=relevance&publishedAfter=" + nowArray[0] + "-" + nowArray[1] + "-" + nowArray[2] + "T00%3A00%3A00Z&q=";
+    
+    link += topic;
+    if(search){
+        var array = search.split(" ");
+        for(var i = 1; i < array.length; i++){
+            link += "%20" + array[i];
+        };
+    };
+    
+    link += "&type=video&videoDuration=short&videoEmbeddable=true&key=AIzaSyALijQA6Nkc_HNMR6qFP0T1IN-cAO0Il2o";
+    const json = await getYouTubeJSON(link);
+    console.log(json);
+    var baseLink = "https://www.youtube.com/embed/";
+    var id = json.items[0].id.videoId;
+    baseLink += id;
+    console.log(baseLink);
+    return baseLink;
+};
+searchYoutube("Finance%20News", "");
+
 // ------------- index functions ----------
 
+var time = "" + nowArray[0] + nowArray[1] + nowArray[2];
 var article1 = document.querySelector("#article1");
 var article2 = document.querySelector("#article2");
 var videoOne = document.querySelector("#video1");
@@ -71,20 +145,122 @@ async function createVideosIndex(){
 
     videoOne.setAttribute("src", videoURL);
 }
+if(article1){
+    createArticlesIndex();
+    createVideosIndex();
+};
 
-createArticlesIndex();
-createVideosIndex();
 
 // -------------- politics functions ------------
+var politicsArticle1 = document.querySelector("#article1Politics");
+var politicsArticle2 = document.querySelector("#article2Politics");
 
+
+async function createArticlesPolitics(){
+    var json = await NYTimesSearch("World", "", time, "");
+    var article1 = json[0].web_url;
+    var article2 = json[1].web_url;
+
+    article1 = makeNYIframe(article1);
+    article2 = makeNYIframe(article2);
+    
+    politicsArticle1.setAttribute("src", article1);
+    politicsArticle2.setAttribute("src", article2);
+}
+if(politicsArticle1){
+    createArticlesPolitics();
+};
 
 // -------------- science functions ------------
+var scienceArticle1 = document.querySelector("#article1Science");
+var scienceArticle2 = document.querySelector("#article2Science");
+
+
+async function createArticlesScience(){
+    var json = await NYTimesSearch("Science", "", time, "");
+    var article1 = json[0].web_url;
+    var article2 = json[1].web_url;
+
+    article1 = makeNYIframe(article1);
+    article2 = makeNYIframe(article2);
+    
+    scienceArticle1.setAttribute("src", article1);
+    scienceArticle2.setAttribute("src", article2);
+}
+if(scienceArticle1){
+    createArticlesScience();
+};
 
 
 // -------------- sports functions ------------
+var sportsArticle1 = document.querySelector("#article1Sports");
+var sportsArticle2 = document.querySelector("#article2Sports");
+
+
+async function createArticlesSports(){
+    var json = await NYTimesSearch("Sports", "", time, "");
+    var article1 = json[0].web_url;
+    var article2 = json[1].web_url;
+
+    article1 = makeNYIframe(article1);
+    article2 = makeNYIframe(article2);
+    
+    sportsArticle1.setAttribute("src", article1);
+    sportsArticle2.setAttribute("src", article2);
+}
+if(sportsArticle1){
+    createArticlesSports();
+};
 
 
 // -------------- tech functions ------------
+var techArticle1 = document.querySelector("#article1Tech");
+var techArticle2 = document.querySelector("#article2Tech");
+var techVideo = document.querySelector("#trending-tech");
 
+
+async function createArticlesTech(){
+    var json = await NYTimesSearch("Technology", "", time, "");
+    var article1 = json[0].web_url;
+    var article2 = json[1].web_url;
+
+    article1 = makeNYIframe(article1);
+    article2 = makeNYIframe(article2);
+    
+    techArticle1.setAttribute("src", article1);
+    techArticle2.setAttribute("src", article2);
+
+    var YTlink = await searchYoutube("Technology%20News", "");
+    techVideo.setAttribute("src", YTlink);
+}
+
+if(techArticle1){
+    createArticlesTech();
+
+}
 
 // -------------- finance functions ------------
+var financeArticle1 = document.querySelector("#article1Finance");
+var financeArticle2 = document.querySelector("#article2Finance");
+var financeVideo = document.querySelector("#trending-finance");
+
+async function createArticlesFinance(){
+    var json = await NYTimesSearch("Your%20Money", "", time, "");
+    console.log(json);
+    var article1 = json[0].web_url;
+    if(json[1]){
+        var article2 = json[1].web_url;
+        article2 = makeNYIframe(article2);
+        financeArticle2.setAttribute("src", article2);
+    };
+    
+    article1 = makeNYIframe(article1);
+    financeArticle1.setAttribute("src", article1);
+
+    var YTlink = await searchYoutube("Finance%20News", "");
+    financeVideo.setAttribute("src", YTlink);
+    
+};
+if(financeArticle1){
+    createArticlesFinance();
+};
